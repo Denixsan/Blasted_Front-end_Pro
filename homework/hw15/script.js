9505ee33cb349208abe5629287e07dfb4ff2fb71
Denixsan <denixsan@gmail.com>
2023-04-25 20:52:11 +0300

    commit 7

--- homework/hw15/script.js
+++ homework/hw15/script.js
@@ -1,13 +1,10 @@
 function sum() {
-    let summa = 0;
-    return function(num) {
-    for (let i = 0; i < num; i++) {
-        summa++;
-    }
-    return summa;
-  }
+  let summa = 0;
+  return function (num) {
+    return (summa += num);
+  };
 }
-  let plus = sum();
-  console.log(plus(3));
-  console.log(plus(5));
-  console.log(plus(20));
+let plus = sum();
+console.log(plus(3));
+console.log(plus(5));
+console.log(plus(20));
