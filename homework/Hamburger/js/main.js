4ceb8654e3a56e9497dfb311eba45cbead1d1bab
Denixsan <denixsan@gmail.com>
2023-06-20 14:18:17 +0300

    commit 17

---
+++ homework/Hamburger/js/main.js
@@ -0,0 +1,5 @@
+class Hamburger {
+    constructor(hSize, hFilling, hAdditive) {
+        
+    }
+}
--- homework/Student/js/main.js
+++ homework/Student/js/main.js
@@ -12,52 +12,52 @@ class Student {
         `Year of birth: ${this.studentBirthYear}; ` +
         `Marks: ${this.studentMarks}`
     );
-    this.average = function () {
-      return (
-        this.studentMarks.reduce((a, b) => a + b, 0) /
-          this.studentMarks.length || 0
-      );
-    };
-    this.age = function () {
-      return new Date().getFullYear() - this.studentBirthYear;
-    };
-    console.log(`Average mark: ${this.average()}; Age: ${this.age()}`);
-    // this.absent = function () {
-    //     this.sAttendance[i] = false;
-    // };
-    // this.present = function () {
-    //     this.sAttendance[i] = true;
-    // };
+    console.log(`Average mark: ${this.average}; Age: ${this.age}`);
     this.sAttendance = new Array(25);
     for (let i = 0; i < this.sAttendance.length; i++) {
       let numGen = +Math.random();
       if (numGen < 0.8) {
-        // this.present();
-        this.sAttendance[i] = true;
+        this.present;
+        // this.sAttendance[i] = true;
       } else {
-        // this.absent();
-        this.sAttendance[i] = false;
+        this.absent;
+        // this.sAttendance[i] = false;
       }
       // this.sAttendance.fill().map((x,i) => (numGen === 0) ? this.absent() : this.present());
     }
     console.log(this.sAttendance);
-    this.summary = function () {
-      console.log(this.sAttendance.filter((x) => x == true));
-      this.sAvgAttendance = +(
-        this.sAttendance.filter((x) => x == true).length /
-        this.sAttendance.length
-      );
-      console.log(`Percentage of attendance: ${this.sAvgAttendance}`);
-      if (this.average() < 90 && this.sAvgAttendance < 0.9) {
-        console.log(`Holy shiet! You're completely lashpet! Get out!`);
-      } else if (this.average() < 90 || this.sAvgAttendance < 0.9) {
-        console.log(`You're lashpet, but not completely.`);
-      } else {
-        console.log(`You're really krasavchik! Well done!`);
-      }
-    };
-    this.summary();
+    console.log(this.summary);
   }
+  get average() {
+    return (
+      this.studentMarks.reduce((a, b) => a + b, 0) /
+        this.studentMarks.length || 0
+    );
+  };
+  get age() {
+    return new Date().getFullYear() - this.studentBirthYear;
+  };
+  get absent() {
+    this.sAttendance[i] = false;
+  };
+  get present() {
+    this.sAttendance[i] = true;
+  };
+  get summary() {
+    console.log(this.sAttendance.filter((x) => x == true));
+    this.sAvgAttendance = +(
+      this.sAttendance.filter((x) => x == true).length /
+      this.sAttendance.length
+    );
+    console.log(`Percentage of attendance: ${this.sAvgAttendance}`);
+    if (this.average < 90 && this.sAvgAttendance < 0.9) {
+      console.log(`Holy shiet! You're completely lashpet! Get out!`);
+    } else if (this.average < 90 || this.sAvgAttendance < 0.9) {
+      console.log(`You're lashpet, but not completely.`);
+    } else {
+      console.log(`You're really krasavchik! Well done!`);
+    }
+  };
 }
 
 const student1 = new Student(`Sus`, `Pup`, `1989`, [98, 78, 66, 75, 64]);
